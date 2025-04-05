fbcffc9246909e5026136ddeb38152d242e72ebc
Robert Best <chessscholar@gmail.com>
2025-04-05 10:45:42 -0400

    Push up for Paul again

---
+++ scripts/build-client.js
@@ -0,0 +1,17 @@
+import * as esbuild from 'esbuild'
+import fs from 'node:fs/promises'
+
+
+let results = await esbuild.build({
+  entryPoints: ['./src/chess-console-stockfish.js'],
+  bundle: true,
+  // minify: true,
+  sourcemap: true,
+  logLevel: 'info',
+  // platform: 'node',
+  metafile: true,
+  outdir: 'client/',
+})
+
+await fs.writeFile('meta-client.json', JSON.stringify(results.metafile))
+console.log("\n  esbuild metadata written to 'meta-client.json'.")
